765b90f6acf44d65ce6ed1b4839ee1a5225891d3
Joe Roodvoets <jroodvoets6@gmail.com>
2026-02-05 16:48:54 -0500

    Initial Changes

---
+++ src/utils/weatherApi.js
@@ -0,0 +1,39 @@
+export const getWeather = ({ latitude, longitude }, APIkey) => {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`,
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+  });
+};
+
+export const filterWeatherData = (data) => {
+  const result = {};
+  result.city = data?.name || "";
+  const tempF = data?.main?.temp ?? null;
+  result.temp = {
+    F: tempF ?? 999,
+    C: tempF != null ? Math.round(((tempF - 32) * 5) / 9) : 999,
+  };
+  result.type = getWeatherType(result.temp.F);
+  result.condition = data.weather[0].main.toLowerCase();
+  result.isDay = isDay(data.sys, Date.now());
+  return result;
+};
+
+const isDay = ({ sunrise, sunset }, now) => {
+  return sunrise * 1000 < now && now < sunset * 1000;
+};
+
+const getWeatherType = (temperature) => {
+  if (temperature > 86) {
+    return "hot";
+  } else if (temperature >= 66 && temperature < 86) {
+    return "warm";
+  } else {
+    return "cold";
+  }
+};
